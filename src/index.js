78cc5e71d5be215dab0bc5338abc460c9316634f
integrating-magic <jeremy@integratingmagic.io>
2024-12-06 11:28:52 -0500

    updates to FM file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 //The first function. Remove this.
 const btn = document.querySelector("button");
+console.log("Hello");
+
 btn.onclick = function () {
+  console.log("You ran some JavaScript");
   alert("You ran some JavaScript");
 };
